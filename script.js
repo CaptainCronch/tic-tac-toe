a377740f97f03c0e20a30d450cdf5417f338f161
Pedro Proenca <crisdelpedro@gmail.com>
2023-02-03 10:10:37 -0700

    check_win() now outputs only X

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
 let x_turn = true
+let win = false
+let cells = $("td")
+let values = ["x", "o"]
+let groups = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
 
-$("td").each(function() {
+
+$(cells).each(function() {
     $(this).on("click", function() {
         if ($(this).text() == ""){
             if (x_turn) {
                 $(this).text("X")
                 x_turn = !x_turn
                 $("#turn-marker").text("O")
-            }
-            else {
+            } else {
                 $(this).text("O")
                 x_turn = !x_turn
                 $("#turn-marker").text("X")
             }
+
+            if (check_win()) {
+                console.log("WIN!!!!!!!")
+            } else {
+                console.log("not yet :(")
+            }
         }
     })
 })
+
+
+function check_win() {
+    for (value in values){
+        for (group in groups) {
+            for (cell in group) {
+                if ($(cells).eq(cell).text() != value){
+                    console.log($(cells).eq(cell).text())
+                    break
+                }
+                console.log("worked")
+                return true;
+            }
+        }
+    }
+    return false
+}
